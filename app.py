c3b9f318a3129f5382cd269e2dfb10b072908b6b
dassanghamitra <das.sanghamitra@gmail.com>
2022-09-26 22:04:42 +0530

    corrected app

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-from app.main import app
- 
-if __name__ == "__main__":
-        app.run()
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
